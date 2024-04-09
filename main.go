695f58f7e4ccb5e4c301e96f7e518a4490f5f418
Victor Senna <vhsenna@gmail.com>
2024-04-09 12:23:22 -0300

    Refactor server initialization to use variable 'r'

--- main.go
+++ main.go
@@ -23,13 +23,13 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	r := http.NewServeMux()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/snippet/view", snippetView)
+	r.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on 8080\n")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal(err)
 	}
 }
