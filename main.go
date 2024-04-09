3bfb3cca4dc5d8774870a6142116b87991e5a18c
Victor Senna <vhsenna@gmail.com>
2024-04-09 16:12:14 -0300

    Add POST method validation to snippetCreate handler

--- main.go
+++ main.go
@@ -19,6 +19,11 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		w.Write([]byte("Method not allowed"))
+		return
+	}
 	w.Write([]byte("Create a new snippet..."))
 }
 
