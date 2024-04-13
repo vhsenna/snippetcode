207f7c23484a875d6a50f94467a3656073c3d79b
Victor Senna <vhsenna@gmail.com>
2024-04-12 21:51:29 -0300

    Refactor code to use a struct 'application' to manage dependencies

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,11 @@ import (
 	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":8080", "HTTP network address")
 	flag.Parse()
@@ -14,14 +19,19 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
 	r := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	r.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	r.HandleFunc("/", home)
-	r.HandleFunc("/snippet/view", snippetView)
-	r.HandleFunc("/snippet/create", snippetCreate)
+	r.HandleFunc("/", app.home)
+	r.HandleFunc("/snippet/view", app.snippetView)
+	r.HandleFunc("/snippet/create", app.snippetCreate)
 
 	srv := &http.Server{
 		Addr:     *addr,
