8e6d856339d1d2e33fb6468826d3fef35829aabf
Victor Senna <vhsenna@gmail.com>
2024-04-14 12:57:12 -0300

    Create a database connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -12,13 +15,32 @@ type application struct {
 	infoLog  *log.Logger
 }
 
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
 func main() {
 	addr := flag.String("addr", ":8080", "HTTP network address")
+	dsn := flag.String("dsn", "web:"+os.Getenv("MYSQL_PASSWORD")+"@/snippetcode?parseTime=true", "MySQL data source name")
 
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -32,7 +54,7 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s\n", *addr)
-	if err := srv.ListenAndServe(); err != nil {
-		errorLog.Fatal(err)
-	}
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
+
 }
