bb6466fc2ed3e051a98321dc7a665782b30be296
Victor Senna <vhsenna@gmail.com>
2024-04-14 13:33:50 -0300

    Create a Snippet model and methods

---
+++ internal/models/snippets.go
@@ -0,0 +1,27 @@
+package models
+
+import "database/sql"
+
+type Snippet struct {
+	ID      int
+	Title   string
+	Content string
+	Created string
+	Expires string
+}
+
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	return 0, nil
+}
+
+func (m *SnippetModel) Get(id int) (*Snippet, error) {
+	return nil, nil
+}
+
+func (m *SnippetModel) Latest() ([]*Snippet, error) {
+	return nil, nil
+}
