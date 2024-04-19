c1fd56357d6dfc85e77abaa158713d18c6dae190
Victor Senna <vhsenna@gmail.com>
2024-04-19 17:31:18 -0300

    Initialize go-chi/chi router and add routes for handling requests

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"net/http"
+	"github.com/go-chi/chi"
 
 	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
-	r := http.NewServeMux()
+	r := chi.NewRouter()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	r.Handle("/static/", http.StripPrefix("/static", fileServer))
+	r.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
-	r.HandleFunc("/", app.home)
-	r.HandleFunc("/snippet/view", app.snippetView)
-	r.HandleFunc("/snippet/create", app.snippetCreate)
+	r.Get("/", app.home)
+	r.Get("/snippet/view/{id}", app.snippetView)
+	r.Get("/snippet/create", app.snippetCreate)
+	r.Post("/snippet/create", app.snippetCreatePost)
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
