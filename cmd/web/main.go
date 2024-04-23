cfe03014f8dd1b7c4236bed503ddff594e9fdc3d
Victor Senna <vhsenna@gmail.com>
2024-04-22 22:42:52 -0300

    Start a HTTPS web server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -71,6 +71,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
 	app := &application{
 		errorLog:       errorLog,
@@ -88,6 +89,6 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s\n", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
