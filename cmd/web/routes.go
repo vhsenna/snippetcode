648e58087c9e11374ec504b0910826bcc69aa93b
Victor Senna <vhsenna@gmail.com>
2024-05-06 23:21:02 -0300

    Restricting access for any user that isn’t logged in

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -22,13 +22,16 @@ func (app *application) routes() http.Handler {
 
 	r.Method(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 	r.Method(http.MethodGet, "/snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
-	r.Method(http.MethodGet, "/snippet/create", dynamic.ThenFunc(app.snippetCreate))
-	r.Method(http.MethodPost, "/snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 	r.Method(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
 	r.Method(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
 	r.Method(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
 	r.Method(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
-	r.Method(http.MethodPost, "/user/logout", dynamic.ThenFunc(app.userLogoutPost))
+
+	protected := dynamic .Append (app .requireAuthentication )
+
+	r.Method(http.MethodGet, "/snippet/create", protected.ThenFunc(app.snippetCreate))
+	r.Method(http.MethodPost, "/snippet/create", protected.ThenFunc(app.snippetCreatePost))
+	r.Method(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogoutPost))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	return standard.Then(r)
