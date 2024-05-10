dc4e55adc536d5c2e6fe76f8b1244beff97d11a4
Victor Senna <vhsenna@gmail.com>
2024-05-10 18:50:10 -0300

    Use the new assert.Equal() helper to compare the expected and actual values

--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"testing"
 	"time"
+
+	"github.com/vhsenna/snippetcode/internal/assert"
 )
 
 func TestHumanDate(t *testing.T) {
@@ -31,9 +33,8 @@ func TestHumanDate(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			hd := humanDate(tt.tm)
-			if hd != tt.want {
-				t.Errorf("got %q; want %q", hd, tt.want)
-			}
+
+			assert.Equal(t, hd, tt.want)
 		})
 	}
 }
